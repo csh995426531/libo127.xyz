b0f8a9e54c747e6a576fab7746416ec493f56438
csh995426531 <15670297001@163.com>
2019-03-16 17:07:12 +0800

    上传项目

---
+++ assets/AppAsset.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main application asset bundle.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class AppAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+//        'css/site.css',
+        'css/bootstrap.css',
+        'css/font-awesome.css',
+        'css/skdslider.css',
+        'css/style.css',
+    ];
+    public $js = [
+        'js/bootstrap.min.js',
+        'js/easing.js',
+        'js/jquery-1.11.1.min.js',
+        'js/minicart.min.js',
+        'js/move-top.js',
+        'js/responsiveslides.min.js',
+        'js/skdslider.min.js',
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+    ];
+
+    //定义按需加载JS方法，注意加载顺序在最后
+    public static function addScript($view, $jsfile) {
+        $view->registerJsFile($jsfile, [AppAsset::className(), 'depends' => 'api\assets\AppAsset']);
+    }
+
+    //定义按需加载css方法，注意加载顺序在最后
+    public static function addCss($view, $cssfile) {
+        $view->registerCssFile($cssfile, [AppAsset::className(), 'depends' => 'api\assets\AppAsset']);
+    }
+}
